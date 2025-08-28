a08a57dc4247083a30c9cf389aea84bf7de04bfe
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-28 15:03:21 +0300

    Created the user-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header],
+  imports: [Header, UserInputComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
